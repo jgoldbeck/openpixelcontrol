7574de0d661910da2678cbe94a8bc9934190272a
Michael Dewberry <michael.dewberry@autodesk.com>
2014-07-19 04:39:07 -0400

    Rough placement of railing cove and roofline lights in layout

--- layouts/cp_tower.js
+++ layouts/cp_tower.js
@@ -4,12 +4,10 @@ var striptypes = [
 	{ group: "middle-ccw", p0: [0.016, 0.855, 4.143], p1: [0.824, 0.238, 7.702], pixels: 113, radialrepeat: 12 },
 	{ group: "top-cw", p0: [0.582, 0.607, 7.788], p1: [0.552, 0.956, 8.961], pixels: 40, radialrepeat: 12 },
 	{ group: "top-ccw", p0: [0.607, 0.582, 7.788], p1: [0.956, 0.552, 8.961], pixels: 40, radialrepeat: 12 },
-    /*
-    { group: "railing-cove", p0: [0, 0, 0], p1: [0, 0, 0], quad: [[0,0,0], [0,0,0], [0,0,0], [0,0,0]], pixels: 1, radialrepeat: 12 },
-	{ group: "roofline", p0: [0, 0, 0], p1: [0, 0, 0], pixels: 70, radialrepeat: 6 },
-	*/
-	{ group: "spire-outer", p0: [0, 0.159, 13.761], pixels: 1, radialrepeat: 30, zrepeat: 16 },
-	{ group: "spire-inner", p0: [0, 0.079, 13.761], pixels: 1, radialrepeat: 15, zrepeat: 16 } 
+    { group: "railing-cove", p0: [-1.25, 0, 10.011], quad: [[-1.204, 0.34, 10.011], [-1.204, 0.34, 9.301], [-1.204, -0.34, 9.301], [-1.204, -0.34, 10.011]], pixels: 1, radialrepeat: 12 },
+	{ group: "roofline", p0: [-1.204, 0.68, 11.261], p1: [-1.204, -0.68, 11.261], pixels: 70, radialrepeat: 6, arch: 0.5 },
+	{ group: "spire-outer", p0: [0, 0.159, 12.7], pixels: 1, radialrepeat: 30, zrepeat: 16 },
+	{ group: "spire-inner", p0: [0, 0.079, 12.7], pixels: 1, radialrepeat: 15, zrepeat: 16 } 
 ];
 
 var center = [0, 0, 0];
@@ -52,6 +50,9 @@ for (var tt = 0; tt < striptypes.length; tt++) {
 					p = p0;
 				}
 				p[2] = p[2] + 0.15 * zz;
+				if ("arch" in type) {
+					p[2] = p[2] + (1 - Math.abs(pp - type.pixels/2) / type.pixels/2) * type.arch;
+				}
 				var item = {
 					point: p,
 					group: type.group
