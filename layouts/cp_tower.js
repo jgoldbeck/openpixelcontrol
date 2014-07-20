92d7da57af35f743f78d49bf98fd37e141c9eb8c
Michael Dewberry <michael.dewberry@autodesk.com>
2014-07-19 23:53:30 -0400

    Cosmic Praise: more sophisticated ray particle demo

--- layouts/cp_tower.js
+++ layouts/cp_tower.js
@@ -1,10 +1,10 @@
 var striptypes = [
-    { group: "base-blast", p0: [0.141, 1.062, 4.062], quad: [[0.052, 0.862, 3.986], [0.02, 1.788, 0], [0.314, 1.73, 0], [0.23, 0.862, 3.986]], pixels: 1, radialrepeat: 36 },
+    { group: "base", quad: [[0.052, 0.862, 3.986], [0.02, 1.788, 0], [0.33, 1.757, 0], [0.23, 0.862, 3.986]], pixels: 1, radialrepeat: 36 },
 	{ group: "middle-cw", p0: [0.016, 0.855, 4.143], p1: [-0.82, 0.254, 7.713], pixels: 113, radialrepeat: 12 },
 	{ group: "middle-ccw", p0: [0.016, 0.855, 4.143], p1: [0.824, 0.238, 7.702], pixels: 113, radialrepeat: 12 },
 	{ group: "top-cw", p0: [0.582, 0.607, 7.788], p1: [0.552, 0.956, 8.961], pixels: 40, radialrepeat: 12 },
 	{ group: "top-ccw", p0: [0.607, 0.582, 7.788], p1: [0.956, 0.552, 8.961], pixels: 40, radialrepeat: 12 },
-    { group: "railing-cove", p0: [-1.25, 0, 10.011], quad: [[-1.204, 0.34, 10.011], [-1.204, 0.34, 9.301], [-1.204, -0.34, 9.301], [-1.204, -0.34, 10.011]], pixels: 1, radialrepeat: 12 },
+    { group: "railing", quad: [[-1.204, 0.34, 10.011], [-1.204, 0.34, 9.301], [-1.204, -0.34, 9.301], [-1.204, -0.34, 10.011]], pixels: 1, radialrepeat: 12 },
 	{ group: "roofline", p0: [-1.204, 0.68, 11.261], p1: [-1.204, -0.68, 11.261], pixels: 70, radialrepeat: 6, arch: 0.5 },
 	{ group: "spire-outer", p0: [0, 0.159, 12.7], pixels: 1, radialrepeat: 30, zrepeat: 16 },
 	{ group: "spire-inner", p0: [0, 0.079, 12.7], pixels: 1, radialrepeat: 15, zrepeat: 16 } 
@@ -40,23 +40,25 @@ for (var tt = 0; tt < striptypes.length; tt++) {
 	for (var zz = 0; zz < zrepeat; zz++) {
 		for (var ii = 0; ii < rrepeat; ii++) {
 			var r = ii * Math.PI * 2 / type.radialrepeat;
-			var p0 = rotate(type.p0, r, center);
 			for (var pp = 0; pp < type.pixels; pp++) {
-				var p;
-				if ("p1" in type) {
-					var p1 = rotate(type.p1, r, center);
-					p = lerp3(p0, p1, pp / type.pixels);
-				} else {
-					p = p0;
-				}
-				p[2] = p[2] + 0.15 * zz;
-				if ("arch" in type) {
-					p[2] = p[2] + (1 - Math.abs(pp - type.pixels/2) / type.pixels/2) * type.arch;
-				}
 				var item = {
-					point: p,
 					group: type.group
 				};
+				if ("p0" in type) {
+					var p;
+					var p0 = rotate(type.p0, r, center);
+					if ("p1" in type) {
+						var p1 = rotate(type.p1, r, center);
+						p = lerp3(p0, p1, pp / type.pixels);
+					} else {
+						p = p0;
+					}
+					p[2] = p[2] + 0.15 * zz;
+					if ("arch" in type) {
+						p[2] = p[2] + (1 - Math.abs(pp - type.pixels/2) / type.pixels/2) * type.arch;
+					}
+					item.point = p;
+				}
 				if ("quad" in type) {
 					item.quad = [
 						rotate(type.quad[0], r, center),
--- python_clients/cosmic_praise.py
+++ python_clients/cosmic_praise.py
@@ -17,6 +17,7 @@ except ImportError:
 
 import opc 
 import color_utils
+import numpy
 
 # remember to 
 # $ sudo pip install colormath
@@ -27,7 +28,6 @@ from colormath.color_conversions import convert_color
 import rtmidi
 from rtmidi.midiutil import open_midiport
 from rtmidi.midiconstants import *
-
 
 #-------------------------------------------------------------------------------
 # parse command line
@@ -103,10 +103,17 @@ print '    parsing layout file'
 print
 
 coordinates = []
+groups = {}
 
 for item in json.load(open(options.layout)):
     if 'point' in item:
         coordinates.append(tuple(item['point']))
+    if 'quad' in item:
+        coordinates.append(tuple(item['quad'][0]))
+    if 'group' in item:
+        if not item['group'] in groups:
+            groups[item['group']] = []
+        groups[item['group']].append(len(coordinates)-1)
     
 
 #-------------------------------------------------------------------------------
@@ -124,17 +131,24 @@ print
 #-------------------------------------------------------------------------------
 # define color effects
 
-def distance(x1, y1, x2, y2):
+def scaledRGBTupleToHSL(s):
+    rgb = sRGBColor(s[0], s[1], s[2], True)
+    return convert_color(rgb, HSLColor)
+    
+def HSLToScaledRGBTuple(hsl):
+    return convert_color(hsl, sRGBColor).get_upscaled_value_tuple()
+
+def distance2d(x1, y1, x2, y2):
     v = (x2 - x1, y2 - y1)
     return math.sqrt(v[0] * v[0] + v[1] * v[1])
 
 def plasma(t, accum, x, y):
     phase = accum
-    stretch = 0.008 + (math.sin(t/20) ** 3 + 1.0) * 3.6
+    stretch = 0.8 + (math.sin(t/20) ** 3 + 1.0) * 200
     p1 = ((math.sin(phase * 1.000) + 0.0) * 2.0, (math.sin(phase * 1.310) + 0.0) * 2.0)
     p2 = ((math.sin(phase * 1.770) + 0.0) * 2.0, (math.sin(phase * 2.865) + 0.0) * 2.0)
-    d1 = distance(p1[0], p1[1], x, y)
-    d2 = distance(p2[0], p2[1], x, y)
+    d1 = distance2d(p1[0], p1[1], x, y)
+    d2 = distance2d(p2[0], p2[1], x, y)
     f = (math.sin(d1 * d2 * stretch) + 1.0) * 0.5
     return f * f
 
@@ -148,8 +162,92 @@ def test_color(t, coord, ii, n_pixels, random_values, accum, trigger):
         dist = math.sqrt(x * x + y * y + z * z)
         p = plasma(t, accum, theta, z)
         c = HSLColor(360.0 * (t % 6)/6.0 + 4 * dist - 30 * p, 0.6 + p/2, 0.5)
-    return convert_color(c, sRGBColor).get_upscaled_value_tuple()
+    return HSLToScaledRGBTuple(c)
 
+def cylinderDistanceSquared(r0, theta0, z0, r1, theta1, z1):
+    x0 = r0 * math.cos(theta0)
+    y0 = r0 * math.sin(theta0)
+    x1 = r1 * math.cos(theta1)
+    y1 = r1 * math.sin(theta1)
+    v = (x1 - x0, y1 - y0, z1 - z0)
+    return v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+
+
+class CosmicRay(object):
+    def __init__(self):
+        self.theta = random.random() * math.pi * 2
+        self.z = 14.5;
+        self.velocity = -(1.8 + random.random() * 1.8)
+        self.rotation = 1.2 * random.random() * (1 if random.random() > 0.5 else -1);
+        self.size = 0.05 + 0.05 * random.random()
+        self.lastUpdate = time.time()
+
+    def tick(self):
+        delta = time.time() - self.lastUpdate
+        self.lastUpdate += delta
+
+        self.z += self.velocity * delta;
+        self.theta = (self.theta + self.rotation * delta) % (2 * math.pi)
+
+rays = []
+
+def updateRays(events, frame_time):
+
+    if random.random() < 1/8:
+        events.append("foo")
+
+    while len(events):
+        e = events.pop()
+        rays.append(CosmicRay())
+
+    for ray in rays:
+        ray.tick()
+        if ray.z < 0:
+            rays.remove(ray)
+
+def rays_color(t, coord, ii, n_pixels, random_values, accum):
+    x, y, z, = coord
+    theta = math.atan2(y, x)
+
+    light = 0
+
+    for ray in rays:
+        d = cylinderDistanceSquared(1.0, ray.theta, ray.z, 1.0, theta, z)
+        if d < ray.size:
+            light += (ray.size - d) / ray.size
+    
+    c = scaledRGBTupleToHSL(miami_color(t, coord, ii, n_pixels, random_values))
+    c.hsl_l += light * 3
+    c.hsl_s = 1 - light
+    return HSLToScaledRGBTuple(c)
+
+def miami_color(t, coord, ii, n_pixels, random_values):
+    # make moving stripes for x, y, and z
+    x, y, z = coord
+    y += color_utils.cos(x - 0.2*z, offset=0, period=1, minn=0, maxx=0.6)
+    z += color_utils.cos(x, offset=0, period=1, minn=0, maxx=0.3)
+    x += color_utils.cos(y - z, offset=0, period=1.5, minn=0, maxx=0.2)
+
+    # rotate
+    x, y, z = y, z, x
+
+    # make x, y, z -> r, g, b sine waves
+    r = color_utils.cos(x, offset=t / 16, period=2.5, minn=0, maxx=1)
+    g = color_utils.cos(y, offset=t / 16, period=2.5, minn=0, maxx=1)
+    b = color_utils.cos(-z, offset=t / 16, period=2.5, minn=0, maxx=1)
+    r, g, b = color_utils.contrast((r, g, b), 0.5, 1.4)
+
+    clampdown = (r + g + b)/2
+    clampdown = color_utils.remap(clampdown, 0.4, 0.5, 0, 1)
+    clampdown = color_utils.clamp(clampdown, 0, 1)
+    clampdown *= 0.8
+    r *= clampdown
+    g *= clampdown
+    b *= clampdown
+
+    g = g * 0.1 + 0.8 * (b + 0.2 * r) / 2 
+
+    return (r*256, g*256, b*256)
 
 #-------------------------------------------------------------------------------
 # send pixels
@@ -164,11 +262,30 @@ accum = 0
 while True:
     frame_time = time.time()
     t = frame_time - start_time
-    trigger = False
-    if len(events) and events[-1][3] > (frame_time - 0.125):
-        trigger = True
-    pixels = [test_color(t*0.6, coord, ii, n_pixels, random_values, accum, trigger) for ii, coord in enumerate(coordinates)]
-    accum = accum + 0.0001 * ( 1.0 + 80.0 * math.pow(math.sin(t/10), 3.0))
+
+    updateRays(events, frame_time)
+    pixels = [rays_color(t*0.6, coord, ii, n_pixels, random_values, accum) for ii, coord in enumerate(coordinates)]
+
+    for index in groups['railing']:
+        hsl = scaledRGBTupleToHSL(pixels[index])
+        hsl.hsl_s = 0.7
+        hsl.hsl_l = 0.3 + 0.4 * hsl.hsl_l
+        hsl.hsl_h = 320 + 40 * hsl.hsl_h / 360;
+        pixels[index] = HSLToScaledRGBTuple(hsl)
+
+    for index in groups['base']:
+        hsl = scaledRGBTupleToHSL(pixels[index])
+        hsl.hsl_s = 0.7
+        hsl.hsl_l = 0.3 + 0.4 * hsl.hsl_l
+        hsl.hsl_h = 280 + 60 * hsl.hsl_h / 360;
+        pixels[index] = HSLToScaledRGBTuple(hsl)
+
+#    trigger = False
+#    if len(events) and events[-1][3] > (frame_time - 0.125):
+#        trigger = True
+#    pixels = [test_color(t*0.6, coord, ii, n_pixels, random_values, accum, trigger) for ii, coord in enumerate(coordinates)]
+#    accum = accum + 0.0001 * ( 1.0 + 80.0 * math.pow(math.sin(t/10), 3.0))
+
     client.put_pixels(pixels, channel=0)
     time.sleep(1 / options.fps)
 
