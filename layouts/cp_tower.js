d9c14c4dd43b2adaee82df3d6a6326f919fa9c7e
Michael Dewberry <michael.dewberry@autodesk.com>
2014-07-24 02:04:12 -0400

    Cosmic Praise: add floodlight to layout

--- layouts/cp_tower.js
+++ layouts/cp_tower.js
@@ -1,13 +1,14 @@
 var striptypes = [
-    { group: "base", quad: [[0.052, 0.862, 3.986], [0.02, 1.788, 0], [ 0.482, 1.72, 0 ], [0.27, 0.819, 3.986]], pixels: 1, radialrepeat: 24 },
 	{ group: "middle-cw", p0: [0.016, 0.855, 4.143], p1: [-0.82, 0.254, 7.713], pixels: 113, radialrepeat: 12 },
 	{ group: "middle-ccw", p0: [0.016, 0.855, 4.143], p1: [0.824, 0.238, 7.702], pixels: 113, radialrepeat: 12 },
 	{ group: "top-cw", p0: [0.582, 0.607, 7.788], p1: [0.552, 0.956, 8.961], pixels: 40, radialrepeat: 12 },
 	{ group: "top-ccw", p0: [0.607, 0.582, 7.788], p1: [0.956, 0.552, 8.961], pixels: 40, radialrepeat: 12 },
-    { group: "railing", quad: [[-1.204, 0.324, 10.011], [-1.204, 0.324, 9.301], [-1.204, -0.324, 9.301], [-1.204, -0.324, 10.011]], pixels: 1, radialrepeat: 12 },
 	{ group: "roofline", p0: [-1.204, 0.68, 11.261], p1: [-1.204, -0.68, 11.261], pixels: 70, radialrepeat: 6, arch: 0.5 },
 	{ group: "spire-outer", p0: [0, 0.159, 12.7], pixels: 1, radialrepeat: 30, zrepeat: 16 },
-	{ group: "spire-inner", p0: [0, 0.079, 12.72], pixels: 1, radialrepeat: 15, zrepeat: 16 } 
+	{ group: "spire-inner", p0: [0, 0.079, 12.72], pixels: 1, radialrepeat: 15, zrepeat: 16 },
+    { group: "base", quad: [[0.052, 0.862, 3.986], [0.02, 1.788, 0], [ 0.482, 1.72, 0 ], [0.27, 0.819, 3.986]], pixels: 1, radialrepeat: 24 },
+    { group: "railing", quad: [[-1.204, 0.324, 10.011], [-1.204, 0.324, 9.301], [-1.204, -0.324, 9.301], [-1.204, -0.324, 10.011]], pixels: 1, radialrepeat: 12 },
+    { group: "floodlight", point: [0, 0, 12.25], size: 0.4, pixels: 1, radialrepeat: 1 }
 ];
 
 var center = [0, 0, 0];
@@ -59,6 +60,9 @@ for (var tt = 0; tt < striptypes.length; tt++) {
 					}
 					item.point = p;
 				}
+				if ("point" in type) {
+					item.point = rotate(type.point, r, center);
+				}
 				if ("quad" in type) {
 					item.quad = [
 						rotate(type.quad[0], r, center),
@@ -66,6 +70,9 @@ for (var tt = 0; tt < striptypes.length; tt++) {
 						rotate(type.quad[2], r, center),
 						rotate(type.quad[3], r, center),
 					];
+				}
+				if ("size" in type) {
+					item.size = type.size;
 				}
 
 				points.push(item);
